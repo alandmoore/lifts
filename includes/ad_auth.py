863dbc6d787f792136a92b914f47b430df9b3523
Alan Moore <me@alandmoore.com>
2014-08-11 22:41:15 -0500

    PEP8, PEP257, other code cleanups

--- includes/ad_auth.py
+++ includes/ad_auth.py
@@ -2,12 +2,30 @@
 # Module for authenticating against Active Directory
 # by Alan Moore
 
-import ldap
+import sys
+if sys.version_info.major > 2:
+    import ldap3 as ldap
+else:
+    import ldap
 from .authenticator import auth_backend
 
 class AD(auth_backend):
-    def __init__(self, host='localhost', port="389", base_dn="",  bind_dn_username="", bind_dn_password="", require_group=None, ssl=False):
-        """Contructor for the connection.  Assumes plaintext LDAP"""
+    def __init__(self, host='localhost', port="389", base_dn="",  
+                 bind_dn_username="", bind_dn_password="", 
+                 require_group=None, ssl=False):
+        """Contruct the connection.  
+
+        Assumes plaintext LDAP.
+        Args:
+        
+        host -- hostname or IP of the LDAP server
+        port -- Port to connect to for LDAP auth
+        base_dn -- The base DN to start searching for users
+        bind_dn_username -- username to user for binding to LDAP
+        bind_dn_password -- bind_dn_username's password
+        require_group -- User must be a member of this group to login
+        ssl -- connect using SSL or not
+        """
         self.error = ""
         self.host = host
         self.base_dn = base_dn
@@ -34,20 +52,29 @@ class AD(auth_backend):
         except ldap.INVALID_CREDENTIALS:
             self.error = "Could not bind to server {}.".format(self.host)
             if self.bind_dn is not None:
-                self.error += "as %s" % self.bind_dn
+                self.error += "as " + self.bind_dn
                 self.con = False
         except ldap.SERVER_DOWN:
             self.error = "Could not make connection to {}.".format(self.host)
 
     def check (self, username=None, password=None):
-        """Given a simple username and password, return true or false if the user is authenticated"""
+        """Attempt to authenticate a username/pw pair
+
+        Returns true if auth succeeded, else false.
+        """
         if self.con:
-            res = self.con.search_s(self.base_dn, ldap.SCOPE_SUBTREE, "sAMAccountName=%s" % username)
+            res = self.con.search_s(
+                self.base_dn, 
+                ldap.SCOPE_SUBTREE, 
+                "sAMAccountName={}".format(username))
         else:
             return False
-        #For some stupid reason, ldap will bind successfully with a valid username and a BLANK PASSWORD, so we have to disallow blank passwords.
+        # For some stupid reason, 
+        # ldap will bind successfully with a valid username and a 
+        # BLANK PASSWORD, so we have to disallow blank passwords.
         if not password:
-            self.error = "Invalid credentials: Login as {} failed.".format(username)
+            self.error = ("Invalid credentials: Login as {} failed."
+                          .format(username))
             return False
 
         if not res[0][0]:
@@ -60,7 +87,8 @@ class AD(auth_backend):
                 self.authenticated_user = username
                 self.authenticated_dn = dn
             except:
-                self.error = "Invalid credentials: Login as {} failed".format(username)
+                self.error = ("Invalid credentials: Login as {} failed"
+                              .format(username))
                 return False
 
         #If you've gotten to this point, the username/password checks out
@@ -72,17 +100,29 @@ class AD(auth_backend):
 
 
     def in_group(self, group):
-        group_res = self.con.search_s(self.base_dn, ldap.SCOPE_SUBTREE, "cn={}".format(group))
+        """Check if the currently authenticated user is a member of <group>
+
+        Returns True if the user is in the group, False if not.
+        Note that indirect membership (member of a group that is a member of
+        the given group) isn't currently detected.
+        """
+        group_res = self.con.search_s(
+            self.base_dn, ldap.SCOPE_SUBTREE, "cn={}".format(group))
         if group_res:
             group_dn = group_res[0][0]
-            if group_dn in self.info_on(self.authenticated_user).get("memberOf"):
+            if group_dn in (
+                    self.info_on(self.authenticated_user)
+                    .get("memberOf", [])):
                 return True
         return False
 
     def info_on(self, username):
-        """Returns ldap information on the given username"""
+        """Return ldap information on the given username"""
         if self.con:
-            res = self.con.search_s(self.base_dn, ldap.SCOPE_SUBTREE, "sAMAccountName={}".format(username))
+            res = self.con.search_s(
+                self.base_dn, 
+                ldap.SCOPE_SUBTREE, 
+                "sAMAccountName={}".format(username))
         else:
             return False
 
@@ -93,11 +133,13 @@ class AD(auth_backend):
         return res[0][1]
 
     def get_auth_user_fullname(self):
+        """Get the full name of the authenticated user from LDAP"""
         if self.authenticated_user:
             return self.info_on(self.authenticated_user).get("name")[0]
         return ""
 
     def get_auth_user_email(self):
+        """Get the email address of the authenticated user from LDAP"""
         if self.authenticated_user:
             email = self.info_on(self.authenticated_user).get("mail", [''])[0]
             return email
--- includes/edirectory_auth.py
+++ includes/edirectory_auth.py
@@ -2,12 +2,29 @@
 # Module for authenticating against eDirectory
 # by Alan Moore
 
-import ldap
+import sys
+if sys.version_info.major > 2:
+    import ldap3 as ldap
+else:
+    import ldap
 from .authenticator import auth_backend
 
 class EDirectory(auth_backend):
-    def __init__(self, host='localhost', port="389", bind_dn="", bind_pw="", require_group = None, ssl=False):
-        """Contructor for the connection.  Assumes plaintext LDAP"""
+    def __init__(self, host='localhost', 
+                 port="389", bind_dn="", 
+                 bind_pw="", require_group = None, 
+                 ssl=False):
+        """Contruct an eDirectory connection object.  
+
+        Assumes plaintext LDAP.
+        Args:
+        host -- The hostname or IP of the directory server
+        port -- The port number to connect to
+        bind_dn -- A DN (username) to bind to the directory as
+        bind_pw -- The bind_dn's password
+        require_group -- Require membership in this group for login
+        ssl -- Connect using SSL or not
+        """
         self.error = ""
         self.host = host
         self.bind_dn = bind_dn
@@ -22,7 +39,7 @@ class EDirectory(auth_backend):
             self.host,
             (port and ":{}".format(port)) or ''
         ])
-        #attempt to connect to the server
+        # attempt to connect to the server
         try:
             self.con = ldap.initialize(self.ldap_url)
             if ssl:
@@ -36,28 +53,36 @@ class EDirectory(auth_backend):
         except ldap.SERVER_DOWN:
             self.error = "Could not make connection to {}.".format(self.host)
     def check (self, username, password):
-        """Given a simple username and password, return true or false if the user is authenticated"""
+        """Authenticate a user
+        
+        Return true if the username & password authenticate, otherwise,
+        return false;
+        """
         if self.con:
-            res = self.con.search_s("", ldap.SCOPE_SUBTREE, "uid=%s" % username)
+            res = self.con.search_s("", ldap.SCOPE_SUBTREE, 
+                                    "uid={}".format(username))
         else:
             return False
-        #For some stupid reason, ldap will bind successfully with a valid username and a BLANK PASSWORD, so we have to disallow blank passwords.
+        # For some stupid reason, ldap will bind successfully 
+        # with a valid username and a BLANK PASSWORD, 
+        # so we have to disallow blank passwords.
         if not password:
-            self.error = "Invalid credentials: Login as %s failed" % username
+            self.error = ("Invalid credentials: Login as {} failed"
+                          .format(username))
             return False
 
         if not res:
-            self.error = "No such user %s" % username
+            self.error = "No such user {}".format(username)
             return False
         else:
             dn = res[0][0]
-            #print (dn)
             try:
                 self.con.simple_bind_s(dn, password)
                 self.authenticated_user = username
                 self.authenticated_dn = dn
             except:
-                self.error = "Invalid credentials: Login as %s failed" % username
+                self.error = ("Invalid credentials: Login as {} failed"
+                              .format(username))
                 return False
         if self.require_group and not (self.in_group(self.require_group)):
             self.error = "Permission Denied"
@@ -65,26 +90,30 @@ class EDirectory(auth_backend):
         return True
 
     def info_on(self, username):
-        """Returns ldap information on the given username"""
+        """Return ldap information on the given username"""
         if self.con:
-            res = self.con.search_s("", ldap.SCOPE_SUBTREE, "uid=%s" % username)
+            res = self.con.search_s("", ldap.SCOPE_SUBTREE, 
+                                    "uid={}".format(username))
         else:
             return False
 
         if not res:
-            self.error = "No such user %s" % username
+            self.error = "No such user {}".format(username)
             return False
 
         return res[0][1]
 
     def get_auth_user_fullname(self):
+        """Get the full name of the authenticated user from LDAP"""
         if self.authenticated_user:
             info = self.info_on(self.authenticated_user)
-            name = "{} {}".format(info.get("givenName", [''])[0], info.get("sn", [''])[0])
+            name = "{} {}".format(info.get("givenName", [''])[0], 
+                                  info.get("sn", [''])[0])
             return name
         return ""
 
     def get_auth_user_email(self):
+        """Get the email address of the authenticated user from LDAP"""
         if self.authenticated_user:
             info = self.info_on(self.authenticated_user)
             email = info.get("mail", [''])[0]
@@ -92,7 +121,13 @@ class EDirectory(auth_backend):
         return None
 
     def in_group(self, group):
-        group_res = self.con.search_s("", ldap.SCOPE_SUBTREE, "cn={}".format(group))
-        if group_res and self.authenticated_dn in group_res[0][1].get("member", []):
-            return True
-        return False
+        """Check if the authenticated user is in the group
+        
+        Return true if they are directly in the group.  Indirect
+        membership is not determined.
+        """
+        group_res = self.con.search_s("", ldap.SCOPE_SUBTREE, 
+                                      "cn={}".format(group))
+        return (group_res and self.authenticated_dn 
+                in group_res[0][1].get("member", []))
+
