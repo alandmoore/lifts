09cf04505f1b539a37ad121ccf8aef4e42598424
Alan D Moore <me@alandmoore.com>
2015-05-07 16:37:03 -0500

    Ajax upload with progress.
    Uploads now use AJAX and show progress as the file uploads.

--- static/js/lifts.js
+++ static/js/lifts.js
@@ -23,10 +23,46 @@ $(document).ready(function(){
     });
 
 
-    // Disable the "send file" button on submit, and put up a message that we're uploading
 
-    $(document).on("submit", "#lifts_form", function(){
+    $(document).on("submit", "#lifts_form", function(e){
+	e.preventDefault()
+	// Disable the "send file" button on submit, and put up a message that we're uploading
+
 	$("INPUT[type=submit]").attr("disabled", "disabled").attr("value", "Please Wait...");
-	$("#messages").html("<h3>Your file is uploading.</h3> This could take a while.  Please be patient, and don't leave this page.");
+	$("#messages").html("<h3>Your file is uploading.</h3><p><span id=percent_done>0</span>% done.  <span id=done_message></span></p> <p>This could take a while.  Please be patient, and don't leave this page.</p>");
+
+	// Do an ajax submit, so we can show upload progress
+	var formdata = new FormData($("#lifts_form")[0]);
+	var action = $("FORM").attr("action");
+
+	$.ajax({
+	    url: action,
+	    data: formdata,
+	    type: 'POST',
+	    processData: false,
+	    contentType: false,
+	    xhr: function(){
+		var xhr = $.ajaxSettings.xhr();
+		console.log(xhr);
+		if (xhr.upload){
+		    xhr.upload.addEventListener("progress", function(e){
+			var percent = 0;
+			if (e.lengthComputable) {
+			    percent = Math.ceil(e.loaded / e.total * 100);
+			}
+			$("#percent_done").html(percent);
+			if(percent === 100){
+			    $("#done_message").html("Uploaded...hang on...");
+			}
+		    });
+		}else{
+		    console.log(xhr);
+		}
+		return xhr;
+	    },
+	    success: function(response){
+		$("#content").html(response);
+	    }
+	});
     });
 });
